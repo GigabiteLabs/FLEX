106de03d3580a9bebb93b7536ff1edd20f7778b3
DanBurkhardt <dan@gigabitelabs.com>
2020-04-12 03:00:24 -0400

    changed package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FLEX",
-            sources: "./Sources/FLEX"),
+            dependencies: []),
         .testTarget(
             name: "FLEXTests",
             dependencies: ["FLEX"]),
