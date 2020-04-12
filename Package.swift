80936d5b14743baf4fa9dd0705e5175ab772e47e
DanBurkhardt <dan@gigabitelabs.com>
2020-04-12 01:46:34 -0400

    update SPM

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FLEX",
-            path: "./Classes",
-            dependencies: []),
+            dependencies: [],
+            path: "./Classes"),
         .testTarget(
             name: "FLEXTests",
             dependencies: ["FLEX"]),
