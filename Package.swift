3aa42c30410b9d332fb4fc8e063662b058c84902
DanBurkhardt <dan@gigabitelabs.com>
2020-04-12 01:44:33 -0400

    added path for FLEX target

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FLEX",
+            path: "./Classes",
             dependencies: []),
         .testTarget(
             name: "FLEXTests",
